d5dd6b6b8e74cb2fe136979588113e1f8beec802
Marcin Nowak <marcin.nowak@techmetria.pl>
2025-09-09 19:49:51 +0200

    Product list and current fuel prices

---
+++ src/main/java/com/github/drnow4u/mcp/mcp_server/OrlenWholesalePriceService.java
@@ -0,0 +1,56 @@
+package com.github.drnow4u.mcp.mcp_server;
+
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Service
+class OrlenWholesalePriceService {
+
+    private final RestTemplate restTemplate = new RestTemplate();
+    private static final String ORLEN_PRODUCTS_API_URL = "https://tool.orlen.pl/api/wholesalefuelprices/Products";
+    private static final String ORLEN_WHOLESALE_FUEL_PRICES_API_URL = "https://tool.orlen.pl/api/wholesalefuelprices";
+    // https://tool.orlen.pl/api/wholesalefuelprices/ByProduct?productId=41&from=2025-01-01&to=2025-09-06
+    private static final String ORLEN_WHOLESALE_FUEL_PRICES_BY_DATE_API_URL = "https://tool.orlen.pl/api/wholesalefuelprices/ByProduct";
+
+    public List<FuelProduct> products() {
+        ResponseEntity<List<FuelProduct>> response = restTemplate.exchange(
+                ORLEN_PRODUCTS_API_URL,
+                HttpMethod.GET,
+                null,
+                new ParameterizedTypeReference<>() {
+                }
+        );
+        return response.getBody();
+    }
+
+    public ResponseEntity<List<WholesaleFuelPriceDto>> currentFuelPrices() {
+        return restTemplate.exchange(
+                ORLEN_WHOLESALE_FUEL_PRICES_API_URL,
+                HttpMethod.GET,
+                null,
+                new ParameterizedTypeReference<>() {
+                }
+        );
+    }
+
+    public List<WholesaleFuelPriceDto> fuelPricesByDate(LocalDate from, LocalDate to) {
+
+        var response = restTemplate.exchange(
+                ORLEN_WHOLESALE_FUEL_PRICES_BY_DATE_API_URL + "?productId=41&from={from}&to={to}",
+                HttpMethod.GET,
+                null,
+                new ParameterizedTypeReference<List<WholesaleFuelPriceDto>>() {
+                },
+                from,
+                to
+        );
+        return response.getBody();
+    }
+
+}
---
+++ src/main/java/com/github/drnow4u/mcp/mcp_server/OrlenWholesalePriceTools.java
@@ -0,0 +1,61 @@
+package com.github.drnow4u.mcp.mcp_server;
+
+import org.springframework.ai.tool.annotation.Tool;
+import org.springframework.ai.tool.annotation.ToolParam;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestClientException;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Component
+class OrlenWholesalePriceTools {
+    private final OrlenWholesalePriceService orlenWholesalePriceService;
+
+    OrlenWholesalePriceTools(OrlenWholesalePriceService orlenWholesalePriceService) {
+        this.orlenWholesalePriceService = orlenWholesalePriceService;
+    }
+
+    @Tool(description = "returns list of wholesale fuels for Orlen")
+    public String[] listWholesaleFuelProducts() {
+        return orlenWholesalePriceService.products()
+                .stream()
+                .map(p -> """
+                        ID: %s | NAME POLISH: %s | NAME ENGLISH: %s | SYMBOL: %s""".formatted(p.id(), p.name(), p.nameEn(), p.symbol()))
+                .toArray(String[]::new);
+    }
+
+    @Tool(description = "returns current wholesale prices for Orlen fuel products")
+    public String[] wholesaleCurrentFuelPrices() {
+        try {
+
+            final var products = orlenWholesalePriceService.products()
+                    .stream()
+                    .collect(java.util.stream.Collectors.toMap(p -> p.symbol(), p -> p));
+
+            final ResponseEntity<List<WholesaleFuelPriceDto>> response = orlenWholesalePriceService.currentFuelPrices();
+
+            if (response.getBody() != null) {
+                return response.getBody().stream()
+                        .map(p -> """
+                                PRODUCT: %s | PRICE: %s PLN | AMOUNT: 1000L | EFFECTIVE DATE: %s | DESCRIPTION POLISH: %s""".formatted(p.productName(), p.value(), p.effectiveDate(), products.get(p.productName()).name()))
+                        .toArray(String[]::new);
+            } else
+                return new String[]{"No data found"};
+
+        } catch (RestClientException e) {
+            System.err.println("Communication error: " + e.getMessage());
+            return new String[]{"Communication error: " + e.getMessage()};
+        }
+    }
+
+    @Tool(description = "returns history wholesale prices for Orlen fuel products per given year")
+    public String[] wholesaleHistoryFuelPrices(@ToolParam(description = "From date in ISO-8601 format") String from, @ToolParam(description = "To date in ISO-8601 format") String to) {
+        return orlenWholesalePriceService.fuelPricesByDate(LocalDate.parse(from), LocalDate.parse(to))
+                .stream()
+                .map(p -> "PRODUCT: %s | PRICE: %s PLN | AMOUNT: 1000L | EFFECTIVE DATE: %s".formatted(p.productName(), p.value(), p.effectiveDate()))
+                .toArray(String[]::new);
+    }
+
+}
